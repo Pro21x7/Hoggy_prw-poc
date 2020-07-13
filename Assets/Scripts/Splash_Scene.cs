eeb98abea36499dea7dbba50eca7840fe4def9e1
Pro21x7 <63231970+Pro21x7@users.noreply.github.com>
2020-07-13 12:36:26 +0530

    Export to GIT
    If this doesn't work; Fresh create a new project

---
+++ Assets/Scripts/Splash_Scene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Splash_Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Up",3);
+        print("after Invoke");
+    }
+
+    // Update is called once per frame
+    public void Up()
+    {
+        SceneManager.LoadScene(1);
+        print("update");        
+    }
+}
